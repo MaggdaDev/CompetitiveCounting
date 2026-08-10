cb536120aa33bbb52807a727f11a56fc520968d2
MaggdaDev <davidbetko05@gmail.com>
2026-08-10 21:51:17 +0200

    Fixed wordle

--- app/src/main/java/CompetitiveCounting/vaults/PrimeVault.java
+++ app/src/main/java/CompetitiveCounting/vaults/PrimeVault.java
@@ -20,7 +20,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 public class PrimeVault extends Vault {
     private final static String GREEN = ":green_square:", YELLOW = ":yellow_square:", WHITE = ":white_large_square:";
-    private final static double SPAWN_CHANCE = 0.02;
+    private final static double SPAWN_CHANCE = 1; // TODO 0.02;
     private final static String RIDDLE = "Guess the correct 4-digit prime number! For each guess, you will be told which digit is " +
             "at the correct spot (" + GREEN + "), at the wrong spot (" + YELLOW + ") or wrong (" + WHITE + "). Only 4-digit primes will be accepted as guesses.";
 
@@ -109,13 +109,14 @@ public class PrimeVault extends Vault {
                         }
                     }
                     for(int i = 0; i < 4; i++) {
-                        if(lettersToAward.contains(submittedChars[i])) {
+                        if (feedback[i] != null) {
+                            continue;
+                        }
+                        if (lettersToAward.contains(submittedChars[i])) {
                             feedback[i] = YELLOW;
                             lettersToAward.remove(lettersToAward.indexOf(submittedChars[i]));
                         } else {
-                            if (feedback[i]==null) {
-                                feedback[i] = WHITE;
-                            }
+                            feedback[i] = WHITE;
                         }
                     }
                     CountingBot.respond(msg, String.join("",feedback));
