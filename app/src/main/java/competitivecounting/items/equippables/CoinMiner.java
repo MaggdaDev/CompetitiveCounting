6b54fba8edc4a4382f1d1880650185ee561449d0
MaggdaDev <davidbetko05@gmail.com>
2026-09-01 13:51:44 +0200

    Nerfed prime coin

--- app/src/main/java/competitivecounting/items/equippables/CoinMiner.java
+++ app/src/main/java/competitivecounting/items/equippables/CoinMiner.java
@@ -10,10 +10,10 @@ public class CoinMiner extends Equippable {
     public static final String NAME = "Coin:pick:Miner";
     public static final String DESCRIPTION = "Equip to gain a small chance to mine a " + Consumables.PRIME_COIN.getName() + " while counting primes.";
     private int coinsMined = 0;
-    private final static double spawnChance = 0.005;
+    private final static double spawnChance = 0.0015;
     public static final String EQUIPPED_DESCRIPTION = "On each prime number that you count, a " + Consumables.PRIME_COIN.getName() +
             " may spawn with a small probability. Anyone with a " + NAME +
-            " equipped may claim it.\n-# " + Consumables.PRIME_COIN.getName() + "s spawned: {0}";
+            " equipped may claim it.\n-# " + Consumables.PRIME_COIN.getName() + "s mined: {0}";
     public static final long COIN_CLAIM_TIMEOUT_SECONDS = 20;
 
     public CoinMiner(Counter owner) {
@@ -44,6 +44,7 @@ public class CoinMiner extends Equippable {
                         .addWaitForEmojiReaction(CountingEmojis.COIN, (msg, emojiReactor) -> {
                             if (emojiReactor.getCollection().containsEquippable(Equippables.COIN_MINER)) {
                                 emojiReactor.getInventory().addItem(Consumables.PRIME_COIN);
+                                ((CoinMiner)emojiReactor.getCollection().getEquippable(Equippables.COIN_MINER)).incrementCoinsMinedStat();
                                 CountingBot.write(msg, "Congratulations, you mined a " + Consumables.PRIME_COIN.getName() + ", " + emojiReactor.getName() + "!");
                                 return true;
                             }
@@ -52,8 +53,9 @@ public class CoinMiner extends Equippable {
                         }, COIN_CLAIM_TIMEOUT_SECONDS, m -> {
                             message.removeReactions(CountingEmojis.COIN).subscribe();
                             new Dialogue()
-                                    .addNpcLine("You have forsaken the wealth of a real " + Consumables.PRIME_COIN.getName() + "...", 2000)
-                                    .addNpcLine("Your temperance is beyond compare.", 2000)
+                                    .addSleep(3)
+                                    .addNpcLine("You have forsaken the wealth of a real " + Consumables.PRIME_COIN.getName() + "...", 3000)
+                                    .addNpcLine("Your temperance is beyond compare.", 3000)
                                     .addRunnable(currMsg -> context.getStreak().getTrophyHandler().spawnTrophy(message, -4))
                                     .play(message);
                             return true;
@@ -61,5 +63,9 @@ public class CoinMiner extends Equippable {
                         .play(message);
             }
         }
+    }
+
+    private void incrementCoinsMinedStat() {
+        coinsMined++;
     }
 }
--- app/src/main/java/competitivecounting/items/equippables/Equippable.java
+++ app/src/main/java/competitivecounting/items/equippables/Equippable.java
@@ -34,4 +34,5 @@ public abstract class Equippable extends Item {
     public void streakDisposed(CountingStreak streak) {
         // Empty
     }
+
 }
